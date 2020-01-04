4494d225a93451275d8b81e9ffac29aeaff4fc03
norouzi <anorouziiii@gmail.com>
2020-01-04 15:28:59 +0330

    add gulp file

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Favorite Grid
 A customizable grid system
 
-
 This grid is inspired by the Bootstrap grid system and the idea behind this system was to be fully customizable.
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+
+// Compile Sass file to css
+sass.compiler = require('node-sass');
+gulp.task('sass', function () {
+    return gulp.src('./*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+// Minify css file
+gulp.task('minify-css', () => {
+    return gulp.src('dist/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/grid.min.css'));
+});
